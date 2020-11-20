1d6a0baf1300adeb8ab5d39c73958c40883e7bc2
ASHOK SASITHARAN <ASHOK2801@hotmail.com>
2020-11-20 11:29:30 -0500

    initial commit

---
+++ src/ca/durhamcollege/Main.java
@@ -0,0 +1,16 @@
+/*
+Title: OOP3200Java-ASasi-JYuan-Lab2
+Name:Ashok Sasitharan 100745484, Jacky Yuan 100520106
+Date: November 20 2020
+Changes: N/A
+ */
+package ca.durhamcollege;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/ca/durhamcollege/WorkTicket.java
@@ -0,0 +1,95 @@
+package ca.durhamcollege;
+
+public class WorkTicket
+{
+    // PRIVATE INSTANCE VARIABLES
+    int ticketNumber;
+    String clientID;
+    int workTicketDay;
+    int workTicketMonth;
+    int workTicketYear;
+    String issueDescription;
+
+    //PUBLIC PROPERTIES (MUTATORS AND ACCESSORS)
+    public int getTicketNumber()
+    {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(int ticketNumber)
+    {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public String getClientID()
+    {
+        return clientID;
+    }
+
+    public void setClientID(String clientID)
+    {
+        this.clientID = clientID;
+    }
+
+    public int getWorkTicketDay()
+    {
+        return workTicketDay;
+    }
+
+    public void setWorkTicketDay(int workTicketDay)
+    {
+        this.workTicketDay = workTicketDay;
+    }
+
+    public int getWorkTicketMonth()
+    {
+        return workTicketMonth;
+    }
+
+    public void setWorkTicketMonth(int workTicketMonth)
+    {
+        this.workTicketMonth = workTicketMonth;
+    }
+
+    public int getWorkTicketYear()
+    {
+        return workTicketYear;
+    }
+
+    public void setWorkTicketYear(int workTicketYear)
+    {
+        this.workTicketYear = workTicketYear;
+    }
+
+    public String getIssueDescription()
+    {
+        return issueDescription;
+    }
+
+    public void setIssueDescription(String issueDescription)
+    {
+        this.issueDescription = issueDescription;
+    }
+
+
+
+    //CONSTRUCTORS
+    //default constructor
+    WorkTicket()
+    {
+        this.ticketNumber=0;
+        this.clientID="";
+        this.workTicketDay=1;
+        this.workTicketMonth=1;
+        this.workTicketYear=2000;
+        this.issueDescription="";
+    }
+
+    //parameterized constructor
+    WorkTicket(final int ticketNumber, final String clientID, final int workTicketDay, final int workTicketMonth, final int workTicketYear, final String issueDescription)
+    {
+
+    }
+    //PUBLIC METHODS
+
+}
